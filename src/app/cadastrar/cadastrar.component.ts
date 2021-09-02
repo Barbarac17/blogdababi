47b855ba7feed715144e9a2ef0707284a1a0d2aa
Barbarac17 <barbara.cirilo17@gmail.com>
2021-09-02 11:26:01 -0300

    inicio feito

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario
+  confirmarSenha: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0, 0)
   }
 
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+
+  }
+
+  cadastrar() {
+
+    if (this.usuario.senha != this.confirmarSenha) {
+      alert('As senhas estão incorretas')
+    } else {
+      // console.log(this.usuario.nome)
+      // console.log(this.usuario.usuario)
+      // console.log(this.usuario.senha)
+      this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+        this.usuario= resp
+        this.router.navigate(['/entrar'])
+        alert('Usuario cadastrado com sucesso!')
+      })
+    }
+  }
 }
